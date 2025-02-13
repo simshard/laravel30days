67f2d0a9ef2900d73cd277e916021c69ac8d95db
Sim <simshard@gmail.com>
2025-02-13 17:52:41 +0000

    mail notify and queues

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Models\Job;
 use App\Models\User;
+use App\Mail\JobPosted;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Mail;
 
 class JobController extends Controller
 {
@@ -41,11 +43,15 @@ class JobController extends Controller
             ]
         );
      
-        Job::create([
+        $job = Job::create([
             'title' =>  request('title'),
             'salary' => request('salary'),
             'employer_id' => 1
         ]);
+
+        Mail::to($job->employer->user)->queue(
+            new JobPosted($job)
+        );
     
         return redirect('/jobs');
     }
